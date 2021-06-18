a6436799d953fda42ca86cd7f49abce0f8e4752b
michaelyeohq@gmail.com <michaelyeohq@gmail.com>
2021-06-19 03:31:45 +0800

    added fifth child

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <div className="childone"> New React App</div>
       <div className="childtwo">
         <div className="childthree">third child</div>
         <div className="childfour">fourth child</div>
+        <div className="childfive">fifth child</div>
       </div>
->>>>>>> master
     </div>
   );
 }
